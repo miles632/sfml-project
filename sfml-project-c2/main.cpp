63a0171030c00d774e870b87489c5a00e2c7f4f3
miles632 <113833646+miles632@users.noreply.github.com>
2023-03-06 22:08:11 +0100

    Add files via upload

--- sfml-project-c2/main.cpp
+++ sfml-project-c2/main.cpp
@@ -1,13 +1,39 @@
 #include <SFML/Graphics.hpp>
 #include "texture_holder.hpp"
 
+#include <SFML/Graphics/Sprite.hpp>
+#include <SFML/Window/Event.hpp>
 #include <iostream>
+#include <stdexcept>
 
 int main()
 {
   sf::RenderWindow window (sf::VideoMode(1280,1080), "Resources");
   window.setFramerateLimit(60);
 
-  // Try to load textures
+  TextureHolder textures;
+  try
+  {
+    textures.LoadResource(Textures::ID::Spaceship, "~/Projects/smfl-project-c2/Spaceship.png");
+    textures.LoadResource(Textures::ID::Landscape, "~/Projects/smfl-project-c2/Space.png");
+    textures.LoadResource(Textures::ID::Missile, "~/Projects/smfl-project-c2/Missile.png");
+  }
+  catch (std::runtime_error& e)
+  {
+    std::cout << "Exception: " << e.what() << std::endl;
+  }
+  // Access resources
+  sf::Sprite landscape(textures.GetTexture(Textures::ID::Landscape));
+  sf::Sprite spaceship(textures.GetTexture(Textures::ID::Spaceship));
+  sf::Sprite missile(textures.GetTexture(Textures::ID::Missile));
 
+  while (window.isOpen())
+  {
+    sf::Event event;
+    while(window.pollEvent(event))
+    {
+      if (event.type == sf::Event::KeyPressed || event.type == sf::Event::Closed)
+        return 0;
+    }
+  }
 }
--- sfml-project-c2/texture_holder.cpp
+++ sfml-project-c2/texture_holder.cpp
@@ -1,6 +1,7 @@
 #include "texture_holder.hpp"
 #include <memory>
 #include <utility>
+#include <cassert>
 
 // Takes in an identifier (key for the resource in the map) and a filename
 void TextureHolder::LoadResource(Textures::ID id, const std::string& filename)
@@ -10,18 +11,34 @@ void TextureHolder::LoadResource(Textures::ID id, const std::string& filename)
   // loads the image to the texture
   if (!texture->loadFromFile(filename))
     throw std::runtime_error ("TextureHolder::load - Failed to load " + filename);
+
   // Inserts Texture into map
-  mTextureMap.insert(std::make_pair(id, std::move(texture)));
+  auto inserted = mTextureMap.insert(std::make_pair(id, std::move(texture)));
+  std::move(texture);
+  assert(inserted.second); // Checks for value, remove after debugging
 }
 
 // Returns a reference to a texture and dereferences the unique ptr
 sf::Texture& TextureHolder::GetTexture(Textures::ID id)
 {
   auto found = mTextureMap.find(id);
+  assert(found != mTextureMap.end());
+
   return *found->second; // the "second" statement returns the value of the kv pair
 }
 sf::Texture& TextureHolder::GetTexture(Textures::ID id) const
 { 
   auto found = mTextureMap.find(id);
-  return *found->second; 
+  assert(found != mTextureMap.end());
+
+  return *found->second;
+}
+
+template <typename Resource, typename Identifier>
+class ResourceHolder
+{
+public:
+  void load (Identifier id, const std:;string& filename);
+  Resource& get(Identifier id);
+
 }
